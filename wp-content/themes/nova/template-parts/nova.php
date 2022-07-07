2147c043a094f93be28b9c2a4259e1ae7bab8e9f
ankita.rai <ankita.rai@twinspark.co>
2022-07-07 17:47:56 +0530

    created custom field,repeater,color picker and make them dynamic

--- wp-content/themes/nova/template-parts/nova.php
+++ wp-content/themes/nova/template-parts/nova.php
@@ -8,19 +8,38 @@
 get_header();?>
 
 
-
 <!-- ======= Hero Section ======= -->
-  <section id="hero" class="hero d-flex align-items-center">
+
+  <section id="hero" class="hero d-flex align-items-center" style="background: url('<?php echo get_template_directory_uri(); ?>/assets/img/hero-bg.jpg') top center;">
     <div class="container">
       <div class="row">
         <div class="col-xl-4">
-          <h2 data-aos="fade-up">Focus On What Matters</h2>
+          <?php $banner_title = get_field('banner_title');
+          if(!empty($banner_title)){?>
+          
+          <h2 data-aos="fade-up"><?php echo $banner_title;?></h2>
+          <?php }else{ ?>
+          <h2 data-aos="fade-up">Hello</h2>
+          <?php }?>
           <blockquote data-aos="fade-up" data-aos-delay="100">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis cum recusandae eum laboriosam voluptatem repudiandae odio, vel exercitationem officiis provident minima. </p>
+            
+            <p> <?php $banner_description = get_field('banner_description');
+          if(!empty($banner_description)){?>
+          
+          <h2 data-aos="fade-up"><?php echo $banner_description;?></h2>
+          <?php }else{ ?>
+          <h2 data-aos="fade-up"> Happy</h2>
+          <?php }?> </p>
           </blockquote>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
             <a href="#about" class="btn-get-started">Get Started</a>
-            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            <a href= <?php $banner_video_link = get_field('banner_video_link');
+          if(!empty($banner_)){?>
+          
+          <h2 data-aos="fade-up"><?php echo $banner_description;?></h2>
+          <?php }else{ ?>
+          <h2 data-aos="fade-up"> </h2>
+          <?php }?></i><span>Watch Video</span></a>
           </div>
 
         </div>
@@ -31,7 +50,8 @@ get_header();?>
   <main id="main">
     <?php get_template_part('template-parts/common/nova-about-section'); ?>
 
-    <?php get_template_part('template-parts/common/nova-services-section'); ?>
+        <?php get_template_part('template-parts/common/nova-services-section'); ?>
+
     <!-- ======= Call To Action Section ======= -->
     <section id="call-to-action" class="call-to-action">
       <div class="container" data-aos="fade-up">
@@ -53,64 +73,27 @@ get_header();?>
         <div class="row">
           <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
             <h3>Powerful Features for <br>Your Business</h3>
-
             <div class="row gy-4">
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-store-line" style="color: #ffbb2c;"></i>
-                  <span>Easy Cart Features</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
-                  <span>Sit amet consectetur adipisicing</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
-                  <span>Ipsum Rerum Explicabo</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
-                  <span>Easy Cart Features</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-database-2-line" style="color: #47aeff;"></i>
-                  <span>Easy Cart Features</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
-                  <span>Sit amet consectetur adipisicing</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
-                  <span>Ipsum Rerum Explicabo</span>
-                </div>
-              </div><!-- End Icon List Item-->
-
-              <div class="col-md-6">
-                <div class="icon-list d-flex">
-                  <i class="ri-base-station-line" style="color: #ff5828;"></i>
-                  <span>Easy Cart Features</span>
-                </div>
-              </div><!-- End Icon List Item-->
+                <?php
+                // Check rows exists.
+                if( have_rows('home_business',132) ):
+                  // Loop through rows.
+                  while( have_rows('home_business',132) ) : the_row();
+                  // Load sub field value.
+                  $business_feature_title = get_sub_field('business_feature_title');
+                  $business_feature_icon = get_sub_field('business_feature_icon');
+                  $business_feature_icon_color = get_sub_field('business_feature_icon_color');
+                ?>
+                  <div class="col-md-6">
+                    <div class="icon-list d-flex">
+                      <i class="<?php echo $business_feature_icon;?>" style="color: <?php echo $business_feature_icon_color;?>;"></i>
+                      <span><?php echo $business_feature_title;?></span>
+                    </div>
+                  </div><!-- End Icon List Item-->
+                <?php 
+                    endwhile;
+                endif;
+                ?>
             </div>
           </div>
           <div class="col-lg-5 position-relative" data-aos="fade-up" data-aos-delay="200">
